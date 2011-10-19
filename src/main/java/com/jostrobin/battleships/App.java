f80d79d533b715b2433bf6f3f09ebb83bcd77e54
Robin Wyss <rowyss@gmail.com>
2011-10-19 16:07:17 +0200

    First commit

---
+++ src/main/java/com/jostrobin/battleships/App.java
@@ -0,0 +1,16 @@
+package com.jostrobin.battleships;
+
+/**
+ * This is the entry point of the application.
+ *
+ * @author rowyss
+ * Date: 18.10.11 Time: 22:33
+ */
+public class App
+{
+	public static void main(String... args)
+	{
+
+	}
+
+}
