a7bebdc0f90365e0d8a86b847a4cad67937bacce
joscht <thomasjost.05@gmail.com>
2012-01-17 16:23:56 +0100

    battlefieldpanel contentpanel minimumsize

--- battleships-presentation/src/main/java/com/jostrobin/battleships/view/panels/BattleFieldPanel.java
+++ battleships-presentation/src/main/java/com/jostrobin/battleships/view/panels/BattleFieldPanel.java
@@ -15,22 +15,30 @@
 
 package com.jostrobin.battleships.view.panels;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.*;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jostrobin.battleships.common.data.AttackResult;
 import com.jostrobin.battleships.common.data.Cell;
 import com.jostrobin.battleships.common.data.enums.CellType;
 import com.jostrobin.battleships.view.components.CellComponent;
 import com.jostrobin.battleships.view.listeners.SelectionListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author rowyss
@@ -76,6 +84,7 @@ public class BattleFieldPanel extends JPanel implements ActionListener
         }
         Dimension dimension = new Dimension(width * CellComponent.CELL_SIZE, height * CellComponent.CELL_SIZE);
         contentPanel.setPreferredSize(dimension);
+        contentPanel.setMinimumSize(dimension);
         setMinimumSize(dimension);
         GridBagConstraints fieldConstraints = new GridBagConstraints();
         fieldConstraints.gridy = 1;
